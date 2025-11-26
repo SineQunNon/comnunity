af1040b4dd1a567ef6032cba40ebfaa5a308bd64
dleck28@gmail.com <SineQunNon>
2025-11-26 14:29:10 +0900

    fix: @Transactional 추가

--- src/main/java/vet/webboard/service/PostService.java
+++ src/main/java/vet/webboard/service/PostService.java
@@ -2,6 +2,7 @@ package vet.webboard.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import vet.webboard.domain.Member;
 import vet.webboard.dto.request.PostCreateRequest;
 import vet.webboard.dto.response.PostResponse;
@@ -11,11 +12,13 @@ import vet.webboard.repository.PostRepository;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class PostService {
     private final PostRepository postRepository;
     private final MemberRepository memberRepository;
     private final PostImageRepository postImageRepository;
 
+    @Transactional
     public PostResponse createPost(PostCreateRequest request, Long memberId) {
         Member member = memberRepository.findById(memberId)
                 .orElseThrow(() -> new IllegalArgumentException("존재하지 않은 회원입니다."));
